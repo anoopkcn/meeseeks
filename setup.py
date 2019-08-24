5e255ae3715de7f8e4bf49bbce4975cda9ee54d4
Anoop Chandran <anoopkcn@gmail.com>
2019-08-24 22:34:05 +0200

    Setup improved with readme and manifest files

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
-setup(name='meeseeks',
-      version='0.1',
-      description='Existance is pain',
-      url='http://github.com/anoopkcn/meeseeks',
-      author='Anoop Chandran',
-      author_email='anoopkcn@gmail.com',
-      license='MIT',
-      packages=['meeseeks'],
-      zip_safe=False)
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='meeseeks',
+    version='0.1',
+    description='Existance is pain',
+    url='http://github.com/anoopkcn/meeseeks',
+    author='Anoop Chandran',
+    author_email='anoopkcn@gmail.com',
+    license='MIT',
+    packages=['meeseeks'],
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+)
