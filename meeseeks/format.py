0e2b20df7a6786f61804340133ca350b7ded8b8e
Anoop Chandran <anoopkcn@gmail.com>
2019-08-25 13:59:44 +0200

    black formating instead of pep8

--- meeseeks/format.py
+++ meeseeks/format.py
@@ -1,5 +1,5 @@
-def strip(list, sep=' '):
-    '''returns a list without brakets[]
+def strip(list, sep=" "):
+    """returns a list without brakets[]
 
     For using python list's in other programs
 
@@ -10,12 +10,12 @@ def strip(list, sep=' '):
     >>> a = [1,2,3,4,5]
     >>> print(strip(a))
     >>> 1 2 4 5
-    '''
-    return f'{sep}'.join(map(str, list))
+    """
+    return f"{sep}".join(map(str, list))
 
 
 def commas(N):
-    '''returns a number separated with commas
+    """returns a number separated with commas
 
     Number is returend in a readable format...
     ... with commas separating the digits 
@@ -27,11 +27,12 @@ def commas(N):
     >>> a = 12345
     >>> print(commas(a))
     >>> 12,345
-    '''
-    return '{:,}'.format(N)
+    """
+    return "{:,}".format(N)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # tests
     print(strip([1, 2, 3, 4, 5]))
     print(commas(20000))
+    print("")
