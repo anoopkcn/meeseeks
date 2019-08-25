1f21493d833b19a4a7e5012a2eacec8fcd276fc9
Anoop Chandran <anoopkcn@gmail.com>
2019-08-25 15:13:49 +0200

    string to number conversion

--- meeseeks/format.py
+++ meeseeks/format.py
@@ -31,8 +31,58 @@ def with_commas(N):
     return "{:,}".format(N)
 
 
+def is_number(s):
+    """
+    Check if a string can be cast to a float or numeric (integer).
+    Takes a string.
+    Returns True or False
+    """
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+    try:
+        import unicodedata
+
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
+
+
+def to_number(s):
+    """
+    Cast a string to a float or integer. 
+    Tries casting to float first and if that works then it tries casting the 
+    string to an integer.
+    Returns a float, int, or if fails, False. (Where using, it shouldn't ever
+    trigger returning `False` because checked all could be converted first.)
+    """
+    try:
+        number = float(s)
+        try:
+            number = int(s)
+            return number
+        except ValueError:
+            pass
+        return number
+    except ValueError:
+        pass
+    try:
+        import unicodedata
+
+        num = unicodedata.numeric(s)
+        return num
+    except (TypeError, ValueError):
+        pass
+    return False
+
+
 if __name__ == "__main__":
     # tests
-    print(strip([1, 2, 3, 4, 5]))
-    print(commas(20000))
-    print("")
+    print(to_string([1, 2, 3, 4, 5]))
+    print(with_commas(20000))
+    print(is_number("3e-4"))
+    print(to_number("3.12e2"))
